f2dc504de51b1d0c39ba3708a59b5ade031059ab
RubenMonlau <rubenrod@campus.molau.com>
2024-11-25 18:05:21 +0100

    1

--- imprimir_numeros_fils.cpp
+++ imprimir_numeros_fils.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include <thread>
 
-void imprimirImpares() {
-    for (int i = 1; i <= 10; i += 2) {
-        std::cout << "Fil imparell: " << i << std::endl;
-    }
-}
-
 void imprimirPares() {
     for (int i = 2; i <= 10; i += 2) {
         std::cout << "Fil parell: " << i << std::endl;
+    }
+}
+
+void imprimirImpares() {
+    for (int i = 1; i <= 10; i += 2) {
+        std::cout << "Fil imparell: " << i << std::endl;
     }
 }
 
